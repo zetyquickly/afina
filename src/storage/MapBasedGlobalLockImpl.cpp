d0118634bc1a90f9d85a480716b8cf90c91a50e3
John Doe <emilzq@bk.ru>
2018-03-12 16:54:40 +0300

    Refactored: Put = PutIfAbsent | Set

--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -10,63 +10,18 @@ namespace Backend {
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) {
     mut.lock();
-    size_t len, last_value_len, key_len, value_len;
-    // more complex case when changes only the value length
-    key_len = key.size();
-    value_len = value.size();
-    len = key_len + value_len;
-    // if key + val greater than storage then stop
-    if (len > _max_size) {
+    if (PutIfAbsent(key, value)) {
         mut.unlock();
-        return false;
-    }
-    my_map::iterator got = _backend.find(key);
-    // if the key is not in the map then add new entry by that key 
-    if (got == _backend.end()) {
-        // if there's not enough space then pop tail element 
-        while (_max_size - _cur_size < len) {
-            Delete(tail->_key);
-        }
-        Entry *entry = new Entry(key, value, head, nullptr);
-        // in case if the list is empty
-        if (tail == nullptr) {
-            tail = entry;
-        }
-        // in all cases
-        head = entry;
-        // in case if the list wasn't empty
-        if (head != tail) {
-            head->_next->_prev = head;
-        }
-        _backend.insert(std::pair<str_ref, Entry *>(entry->_key, entry));
-    // if the key is already in map rewrite entry's value field
+        return true;
     } else {
-        last_value_len = got->second->_value.size();
-        // if there's not enough space only for the value then pop tail element 
-        while (_max_size - _cur_size - last_value_len < value_len) {
-            Delete(tail->_key);
+        if (Set(key, value)) {
+            mut.unlock();
+            return true;
+        } else {
+            mut.unlock();
+            return false;
         }
-        got->second->_value = value;
-        // decrease current size on last value's length
-        _cur_size -= last_value_len;
-        // and place this very entry to the front of the list
-        if (got->second != head) {
-            if (got->second == tail) {
-                got->second->_prev->_next = nullptr;
-                tail = got->second->_prev;
-            } else {
-                got->second->_next->_prev = got->second->_prev;
-                got->second->_prev->_next = got->second->_next;
-            }
-            got->second->_next = head;
-            head->_prev = got->second;
-            head = got->second;
-        }
-    }
-    // increase current size counter
-    _cur_size += len;
-    mut.unlock();
-    return true;
+    } 
 }
 
 // See MapBasedGlobalLockImpl.h
